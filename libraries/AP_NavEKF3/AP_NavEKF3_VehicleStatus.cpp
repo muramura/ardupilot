717bf718a0f646e76b29d1735f758ca0d946bdb5
murata <ma2maru@gmail.com>
2020-07-19 21:43:00 +0900

    AP_NavEKF3: disable GPS checks when GPS is disabled

--- libraries/AP_NavEKF3/AP_NavEKF3_VehicleStatus.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_VehicleStatus.cpp
@@ -19,6 +19,10 @@ extern const AP_HAL::HAL& hal;
 */
 void NavEKF3_core::calcGpsGoodToAlign(void)
 {
+    if (frontend->_fusionModeGPS == 3) {
+        return;
+    }
+
     if (inFlight && assume_zero_sideslip() && !use_compass()) {
         // this is a special case where a plane has launched without magnetometer
         // is now in the air and needs to align yaw to the GPS and start navigating as soon as possible
